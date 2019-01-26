5224bc1c63ab597b4a9cb6439f28fb86da133087
cristian <cri.gacitua.flores@gmail.com>
2019-01-26 10:52:03 -0300

    Initialize review

--- lib/review_list.dart
+++ lib/review_list.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'review.dart';
 
 class ReviewList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Review("assets/img/girl_traveler.jpg","María Nieves","2 review 2 photo","There is an amazing place in Sri Land"),
+        Review("assets/img/josh_traveler.jpg","Josh jhon","1 review 3 photo","There is an amazing place in Sri Land"),
+        Review("assets/img/traveler_photo.jpg","Cristian Gactiúa","4 review 5 photo","There is an amazing place in Sri Land"),
+      ],
+    );
   }
 
 }
