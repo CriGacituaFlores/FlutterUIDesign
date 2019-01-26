89a2c6d40a897058ac971e9fdd88d409829f61b9
cristian <cri.gacitua.flores@gmail.com>
2019-01-26 11:03:24 -0300

    Gradient decoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'description_place.dart';
 import 'review_list.dart';
+import 'gradient_back.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,11 +29,19 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hola mundo")
-        ),
         //body: new DescriptionPlace("Bahamas", 4, descriptionDummy),
-        body: ReviewList(),
+        //body: ReviewList(),
+        body: Stack(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                DescriptionPlace("Bahamas", 4, descriptionDummy),
+                ReviewList(),
+              ],
+            ),
+            GradientBack(),
+          ],
+        )
       )//MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
